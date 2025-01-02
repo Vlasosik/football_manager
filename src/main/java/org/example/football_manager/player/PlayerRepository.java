7efb3ac45ba0dbbc33a71d1d838af8f006cd19a2
Vlasosik <128188585+Vlasosik@users.noreply.github.com>
2025-01-02 21:52:29 +0200

    created PlayerRepository class interface

---
+++ src/main/java/org/example/football_manager/player/PlayerRepository.java
@@ -0,0 +1,10 @@
+package org.example.football_manager.player;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+import java.util.UUID;
+
+@Repository
+public interface PlayerRepository extends JpaRepository<Player, UUID> {
+}
