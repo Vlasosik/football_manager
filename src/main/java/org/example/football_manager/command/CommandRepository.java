cf89d8b6d07ca7afd2a11fcd5d0735312a63d853
Vlasosik <128188585+Vlasosik@users.noreply.github.com>
2025-01-02 21:47:57 +0200

    created CommandRepository class

---
+++ src/main/java/org/example/football_manager/command/CommandRepository.java
@@ -0,0 +1,10 @@
+package org.example.football_manager.command;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+import java.util.UUID;
+
+@Repository
+public interface CommandRepository extends JpaRepository<Command, UUID> {
+}
