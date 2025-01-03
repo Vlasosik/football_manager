847778c02728e3ddd4e9bb53a5ab8a4cab95cbc3
Vlasosik <128188585+Vlasosik@users.noreply.github.com>
2025-01-03 19:05:52 +0200

    changed annotation for field to CommandDTO

--- src/main/java/org/example/football_manager/command/dto/CommandDTO.java
+++ src/main/java/org/example/football_manager/command/dto/CommandDTO.java
@@ -4,6 +4,7 @@ import jakarta.validation.constraints.*;
 import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
+import org.example.football_manager.command.validation.UniqueNameValidation;
 
 import java.math.BigDecimal;
 import java.util.UUID;
@@ -14,12 +15,14 @@ import java.util.UUID;
 public class CommandDTO {
     @NotNull
     private UUID id;
-    @NotBlank
+    @UniqueNameValidation
     @Size(min = 1, max = 50)
     private String name;
-    @DecimalMin(value = "0.0")
+    @DecimalMin(value = "0.00")
+    @Digits(integer = 10, fraction = 2)
     private BigDecimal balance = BigDecimal.ZERO;
-    @DecimalMin(value = "0.0")
-    @DecimalMax(value = "10.0")
+    @DecimalMin(value = "0.00")
+    @DecimalMax(value = "10.00")
+    @Digits(integer = 5, fraction = 2)
     private BigDecimal commissionRate = BigDecimal.ZERO;
 }
