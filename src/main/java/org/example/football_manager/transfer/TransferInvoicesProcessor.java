6c121095dbe5847233a8216a1fca0d07d0dd5fbb
Vlasosik <128188585+Vlasosik@users.noreply.github.com>
2025-01-03 19:08:46 +0200

    created TransferInvoicesProcessor class where implement main logic in task

---
+++ src/main/java/org/example/football_manager/transfer/TransferInvoicesProcessor.java
@@ -0,0 +1,28 @@
+package org.example.football_manager.transfer;
+
+import org.example.football_manager.command.dto.CommandDTO;
+import org.example.football_manager.player.dto.PlayerDTO;
+import org.springframework.stereotype.Component;
+
+import java.math.BigDecimal;
+import java.math.RoundingMode;
+
+@Component
+public class TransferInvoicesProcessor {
+    public BigDecimal getCostOfTransfer(PlayerDTO playerDTO) {
+        short experienceYear = playerDTO.getExperience();
+        short age = playerDTO.getAge();
+        BigDecimal experienceMonths = BigDecimal.valueOf(experienceYear * 12);
+        return experienceMonths.multiply(BigDecimal.valueOf(100000))
+                .divide(BigDecimal.valueOf(age), 2, RoundingMode.HALF_UP);
+    }
+
+    public BigDecimal getTotalAmountOfTransfer(CommandDTO commandDTO, PlayerDTO playerDTO) {
+        BigDecimal commissionRate = commandDTO.getCommissionRate();
+        BigDecimal costOfTransfer = getCostOfTransfer(playerDTO);
+        BigDecimal commissionAmount = costOfTransfer.multiply(commissionRate)
+                .divide(BigDecimal.valueOf(100), 2, RoundingMode.HALF_UP);
+        return costOfTransfer.add(commissionAmount);
+    }
+
+}
