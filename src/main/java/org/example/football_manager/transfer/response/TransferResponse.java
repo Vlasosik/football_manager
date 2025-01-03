db9a9f255e2668589306336757f5defa7131724d
Vlasosik <128188585+Vlasosik@users.noreply.github.com>
2025-01-03 19:09:21 +0200

    created TransferResponse class.

---
+++ src/main/java/org/example/football_manager/transfer/response/TransferResponse.java
@@ -0,0 +1,15 @@
+package org.example.football_manager.transfer.response;
+
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+@Builder
+public class TransferResponse {
+    private String message;
+    private int statusCode;
+}
