0634c408716607c4ad8dbccbfc6a712e8fe0c681
Vlasosik <128188585+Vlasosik@users.noreply.github.com>
2025-01-04 11:35:59 +0200

    small refactor custom annotation

--- src/main/java/org/example/football_manager/command/validation/UniqueNameValidationImpl.java
+++ src/main/java/org/example/football_manager/command/validation/UniqueNameValidationImpl.java
@@ -3,19 +3,14 @@ package org.example.football_manager.command.validation;
 import jakarta.validation.ConstraintValidator;
 import jakarta.validation.ConstraintValidatorContext;
 import lombok.RequiredArgsConstructor;
-import org.example.football_manager.command.CommandService;
 import org.springframework.stereotype.Component;
 
 @Component
 @RequiredArgsConstructor
 public class UniqueNameValidationImpl implements ConstraintValidator<UniqueNameValidation, String> {
-    private final CommandService commandService;
 
     @Override
     public boolean isValid(String name, ConstraintValidatorContext constraintValidatorContext) {
-        if (name == null || name.trim().isEmpty() || name.equals("null")) {
-            return false;
-        }
-        return !commandService.existsByName(name);
+        return name != null && !name.trim().isEmpty() && !name.equals("null");
     }
 }
